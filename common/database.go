ae0dc7bd1024866444a613491dd484b723721cd3
lomi <1109815645@qq.com>
2023-08-08 16:21:24 +0800

    新增viper进行配置管理，通过yml格式的文件管理配置信息

--- common/database.go
+++ common/database.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"fmt"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"kok/model"
@@ -11,13 +12,13 @@ var DB *gorm.DB
 
 // 数据库连接池
 func InitDB() *gorm.DB {
-	driverName := "mysql"   //数据库类型
-	host := "localhost"     //数据库地址
-	port := "3306"          //数据库端口
-	database := "kok"       //数据库名称
-	username := "root"      //数据库用户名
-	password := "lomi1015." //数据库密码
-	charset := "utf8"       //数据库编码方式
+	driverName := viper.GetString("datasource.driverName") //数据库类型
+	host := viper.GetString("datasource.host")             //数据库地址
+	port := viper.GetString("datasource.port")             //数据库端口
+	database := viper.GetString("datasource.database")     //数据库名称
+	username := viper.GetString("datasource.username")     //数据库用户名
+	password := viper.GetString("datasource.password")     //数据库密码
+	charset := viper.GetString("datasource.charset")       //数据库编码方式
 	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true&loc=Local", username, password, host, port, database, charset)
 
 	db, err := gorm.Open(mysql.New(mysql.Config{DriverName: driverName, DSN: args}), &gorm.Config{})
--- main.go
+++ main.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"kok/common"
+	"os"
 )
 
 func main() {
+	InitConfig()
 	db := common.InitDB() //连接数据库
 	sqldb, _ := db.DB()   //实例化DB()函数
 	defer sqldb.Close()   //延迟关闭数据库连接
 	r := gin.Default()    //默认路由分组
 	r = CollectRoute(r)   //路由对象更新
-	panic(r.Run())        // 监听并在 0.0.0.0:8080 上启动服务
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port)) // 监听并在指定port端口启动服务
+	}
+	panic(r.Run()) // 监听并在 0.0.0.0:8080上启动服务
+}
+
+// 读取项目配置文件
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("Read faild to config ,err:" + err.Error())
+	}
 }
